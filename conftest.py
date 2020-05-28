2ed2e78148ef20c66ca2795ea6bdd54fe60c00eb
9DirtyDaniel9 <57323262+9DirtyDaniel9@users.noreply.github.com>
2020-05-28 22:50:29 +0700

    conftest

---
+++ conftest.py
@@ -0,0 +1,32 @@
+
+import pytest
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from murr_card.models import MurrCard
+
+
+Murren = get_user_model()
+
+
+@pytest.fixture
+def create_murren():
+    user1 = Murren.objects.create_user(username='danl',
+                              password='123k45',
+                              email='integritys@mail.com')
+    client = APIClient()
+    client.force_authenticate(user=user1)
+    user1.save()
+    return user1
+
+
+
+@pytest.fixture
+def create_murr():
+    user1 = Murren.objects.get(pk=1)
+    one_murr = MurrCard.objects.create(
+        title=1,
+        content="Halo, dat's not me",
+        owner=user1,
+    )
+    return one_murr
+
